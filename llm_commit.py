6cdf8532dcacc89d858b88bdbc415ce1efacbb59
Glenn Y. Rolland <glenux@glenux.net>
2025-02-28 13:11:34 +0100

    feat: Update generate_commit_message signature
    - Modify generate_commit_message function signature to make the 'commit_style' parameter optional.
    - Default value for 'commit_style' is now set to None.
    
    Signed-off-by: Glenn Y. Rolland <glenux@glenux.net>

--- llm_commit.py
+++ llm_commit.py
@@ -35,7 +35,7 @@ def get_staged_diff(truncation_limit=4000, no_truncation=False):
         diff = diff[:truncation_limit] + "\n[Truncated]"
     return diff
 
-def generate_commit_message(diff, commit_style, model=None, max_tokens=100, temperature=0.7):
+def generate_commit_message(diff, commit_style=None, model=None, max_tokens=100, temperature=0.7):
     import llm
     from llm.cli import get_default_model
     from llm import get_key
